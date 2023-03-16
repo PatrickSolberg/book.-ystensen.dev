c36976a77d59535900160a645071a0b4c2638f78
Patrick Solberg <patrick.oysteinsen@gmail.com>
2023-03-16 16:40:31 +0100

    add constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Book.Øystensen.dev';
+export const SITE_DESCRIPTION = 'Welcome to my bookshelf. Here you can find all the books I have read and rated.';
+export const HOMEPAGE_URL = 'https://book.oystensen.dev';
