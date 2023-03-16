8b907d742116033f053952a607c93df58884463c
Patrick Solberg <patrick.oysteinsen@gmail.com>
2023-03-16 22:05:54 +0100

    add readtime function

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,11 @@
 export const SITE_TITLE = 'Book.Øystensen.dev';
 export const SITE_DESCRIPTION = 'Welcome to my bookshelf. Here you can find all the books I have read and rated.';
 export const HOMEPAGE_URL = 'https://book.oystensen.dev';
+
+export const getReadingTime = (text: string) => {
+    const wordsPerMinute = 200;
+    const noOfWords = text.split(/\s/g).length;
+    const minutes = noOfWords / wordsPerMinute;
+    const readTime = Math.ceil(minutes);
+    return readTime;
+};
