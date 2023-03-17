9230824663724dc92ae08dec7d7111fbacef39c1
Patrick Solberg <patrick.oysteinsen@gmail.com>
2023-03-17 18:55:59 +0100

    udpate style

--- src/components/ListPosts.tsx
+++ src/components/ListPosts.tsx
@@ -45,10 +45,10 @@ const ListPost = ({ post }: Props) => {
             </a>
             {description && (
                 <div>
-                    <a href={post.url || '404'} className='cursor-pointer hover:underline hover:underline-offset-4 hover:decoration-brand-purple dark:hover:decoration-brand-blue'>
+                    <a href={post.url || '404'} className='cursor-pointer hover:underline hover:underline-offset-4 hover:decoration-cyan-300 dark:hover:decoration-brand-blue'>
                         <h1 className='text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white inline-flex'>{title}</h1>
                     </a>
-                    <p>Author: <a href={author} className='cursor-pointer hover:underline hover:underline-offset-4 hover:decoration-brand-purple dark:hover:decoration-brand-blue'><span className='italic text-xl'>{author || 'unknown'}</span></a></p>
+                    <p>Author: <a href={author} className='cursor-pointer hover:underline hover:underline-offset-4 hover:decoration-cyan-300'><span className='italic text-xl'>{author || 'unknown'}</span></a></p>
                     <p className='truncate italic'>"{description}"</p>
                     <div className='flex gap-4'>
                         <p className=''>Published: {pubDate}</p>
