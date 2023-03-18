129d99166b3ea85d809a129f24e8a6ebc46490ce
Patrick Solberg <patrick.oysteinsen@gmail.com>
2023-03-18 17:52:51 +0100

    add prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), tailwind(), image()]
+  integrations: [mdx(), sitemap(), react(), tailwind(), image(), prefetch()],
+  markdown: {}
 });
